13248da552ec00f032b875bc99aa0e452086002a
Wes Turner <wes@wrd.nu>
2012-09-10 18:59:21 -0500

    Added cornice application scaffold

---
+++ setup.py
@@ -0,0 +1,35 @@
+""" Setup file.
+"""
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.rst')) as f:
+    README = f.read()
+
+
+setup(name='icd10api',
+    version=0.1,
+    description='icd10api',
+    long_description=README,
+    classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pylons",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application"
+    ],
+    keywords="web services",
+    author='',
+    author_email='',
+    url='',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['cornice', 'PasteScript'],
+    entry_points = """\
+    [paste.app_factory]
+    main = icd10api:main
+    """,
+    paster_plugins=['pyramid'],
+)
