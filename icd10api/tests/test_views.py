d5c6f243b9d5ae952e478bc3126b37679fef2f8d
Wes Turner <wes@wrd.nu>
2012-09-15 04:09:24 -0500

    Updated chapter, section, and diag resources:  * Added stub resource descriptions  * Added HTTP redirects for both GET and POST with 'q' parameter  * Added HTTP 404 for empty result sets

--- icd10api/tests/test_views.py
+++ icd10api/tests/test_views.py
@@ -5,27 +5,41 @@ from icd10api import main
 
 class TestICD10piApp(unittest.TestCase):
 
+    def setUp(self):
+        self.app= TestApp(main({}))
+
     def test_root(self):
-        app = TestApp(main({}))
-        app.get('/', status=200)
+        self.app.get('/', status=200)
 
     def test_apichapters(self):
-        app = TestApp(main({}))
-        app.get('/api/chapters', status=200)
+        self.app.get('/api/chapters', status=200)
+    def test_apichapters_query_get(self):
+        self.app.get('/api/chapters', {'q':'1'}, status=307)
+    def test_apichapters_query_post(self):
+        self.app.post('/api/chapters', {'q':'1'}, status=307)
     def test_apichapter(self):
-        app = TestApp(main({}))
-        app.get('/api/chapters/1', status=200)
+        self.app.get('/api/chapters/1', status=200)
+    def test_apichapter_404(self):
+        self.app.get('/api/chapters/not_valid', status=404)
 
     def test_apisections(self):
-        app = TestApp(main({}))
-        app.get('/api/sections', status=200)
+        self.app.get('/api/sections', status=200)
+    def test_apisections_query_get(self):
+        self.app.get('/api/sections', {'q':'a00-a09'}, status=307)
+    def test_apisections_query_post(self):
+        self.app.post('/api/sections', {'q':'a00-a09'}, status=307)
     def test_apisection(self):
-        app = TestApp(main({}))
-        app.get('/api/sections/a00-a09', status=200)
+        self.app.get('/api/sections/a00-a09', status=200)
+    def test_apisection_404(self):
+        self.app.get('/api/sections/not_valid', status=404)
 
     def test_apidiags(self):
-        app=TestApp(main({}))
-        app.get('/api/diags', status=200) # TODO: time
+        self.app.get('/api/diags', status=200) # TODO: time
+    def test_apidiags_query_get(self):
+        self.app.get('/api/diags', {'q':'a00'}, status=307)
+    def test_apidiags_query_post(self):
+        self.app.post('/api/diags', {'q':'a00'}, status=307)
     def test_apidiag(self):
-        app = TestApp(main({}))
-        app.get('/api/diags/a00', status=200)
+        self.app.get('/api/diags/a00', status=200)
+    def test_apidiag_404(self):
+        self.app.get('/api/diags/not_valid', status=404)
--- icd10api/views.py
+++ icd10api/views.py
@@ -2,7 +2,7 @@
 """
 from cornice import Service
 from cornice.resource import resource, view
-
+from pyramid.httpexceptions import HTTPNotFound, HTTPTemporaryRedirect
 
 hello = Service(name='hello', path='/', description="Simplest app")
 
@@ -14,55 +14,77 @@ def get_info(request):
 
 import icd10
 
-@resource(collection_path='/api/chapters', path='/api/chapters/{id}')
+@resource(collection_path='/api/chapters', path='/api/chapters/{id}',
+         description="ICD10 Chapters")
 class Chapter(object):
     def __init__(self, request):
         self.request = request
 
     @view(renderer='json')
     def collection_get(self):
-        return {'chapters': icd10.get_chapters() } # TODO
+        if 'q' in self.request.params:
+            return self.collection_post()
+        return {'chapters': icd10.get_chapters() }
+
+    def collection_post(self):
+        return HTTPTemporaryRedirect(
+                '/api/chapters/%s' % self.request.params.get('q'))
 
     @view(renderer='json')
     def get(self):
         code = self.request.matchdict.get("id").upper()
-        return icd10.get_chapter(code)
+        chapter = icd10.get_chapter(code)
+        if not chapter:
+            raise HTTPNotFound()
+        return chapter
 
 
-@resource(collection_path='/api/sections', path='/api/sections/{id}')
+@resource(collection_path='/api/sections', path='/api/sections/{id}',
+            description="ICD10 Sections")
 class Section(object):
     def __init__(self, request):
         self.request = request
 
     @view(renderer='json')
     def collection_get(self):
-        return {'sections': icd10.get_sections() } # TODO
+        if 'q' in self.request.params:
+            return self.collection_post()
+        return {'sections': icd10.get_sections() }
 
     def collection_post(self):
-        # TODO: redirect to self.get
-        return None
+        return HTTPTemporaryRedirect(
+                '/api/sections/%s' % self.request.params.get('q'))
 
     @view(renderer='json')
     def get(self):
         code = self.request.matchdict.get("id").upper()
-        return icd10.get_section(code)
+        sections = icd10.get_section(code)
+        if not sections:
+            raise HTTPNotFound()
+        return sections
 
 
-@resource(collection_path='/api/diags', path='/api/diags/{id}')
+@resource(collection_path='/api/diags', path='/api/diags/{id}',
+            description="ICD10 Diags")
 class Diag(object):
     def __init__(self, request):
         self.request = request
 
     @view(renderer='json')
     def collection_get(self):
+        if 'q' in self.request.params:
+            return self.collection_post()
         return {'diags': icd10.get_diags() } # TODO: memoize?
 
     def collection_post(self):
-        #TODO: redirect to self.get
-        return None
+        return HTTPTemporaryRedirect(
+                '/api/diags/%s' % self.request.params.get('q'))
 
     @view(renderer='json')
     def get(self):
         code = self.request.matchdict.get("id").upper()
-        return icd10.get_diag(code)
+        diags = icd10.get_diag(code)
+        if not diags:
+            raise HTTPNotFound()
+        return diags
 
